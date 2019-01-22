f4d16f1451bfc6fa5422118ec08a3b977377de14
soumya <soumya@walkterbeaconlab.com>
2019-01-22 22:49:00 +0530

    Initial commit

---
+++ gradienttextview/src/main/java/com/slenka/gradient/color/gradienttextview/GradientTextView.java
@@ -0,0 +1,34 @@
+package com.slenka.gradient.color.gradienttextview;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.text.TextPaint;
+import android.widget.TextView;
+
+public class GradientTextView extends android.support.v7.widget.AppCompatTextView {
+
+
+    public GradientTextView(Context context) {
+        super(context);
+    }
+
+    public void setGradientColor(TextView textView,int startColor, int endColor)
+    {
+
+        TextPaint paint = textView.getPaint();
+        float width = paint.measureText("Gradient Color");
+
+        Shader textShader = new LinearGradient(0, 0, width, textView.getTextSize(),
+                new int[]{
+                        startColor,
+                        endColor,
+                }, null, Shader.TileMode.CLAMP);
+        textView.getPaint().setShader(textShader);
+
+        textView.setTextColor(startColor);
+
+    }
+
+}
